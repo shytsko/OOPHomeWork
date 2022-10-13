71e7aafd5b7e2dd6f5be334e10953788c4595c04
Vladimir Shytsko <shytsko.v@gmail.com>
2022-10-13 19:17:52 +0300

    Http Server Finish

--- src/main/java/gb/HomeWork5/HttpServer/Server.java
+++ src/main/java/gb/HomeWork5/HttpServer/Server.java
@@ -1,3 +1,6 @@
+// Дописать простейший веб сервер, который начали писать на вебинаре, чтобы он отправлял в качестве
+// ответа на запрос содержимлое файла из HTTP-запроса.
+
 package gb.HomeWork5.HttpServer;
 
 
@@ -13,36 +16,44 @@ public class Server {
 
     public static void main(String[] args) {
         try {
+            String wwwPath = new File(".").getCanonicalPath() + "/www";
             ServerSocket serverSocket = new ServerSocket(8080);
-            int count = 0;
+            System.out.println("Waiting for connection ...");
+            boolean serverRun = true;
 
-            for(;;) {
+            while(serverRun) {
                 Socket socket = serverSocket.accept();
                 System.out.println("Client connected");
-                BufferedReader reader = new BufferedReader(
+                 BufferedReader reader = new BufferedReader(
                         new InputStreamReader(
                                 socket.getInputStream(), StandardCharsets.UTF_8));
 
                 PrintWriter writer = new PrintWriter(
                         socket.getOutputStream());
 
+                System.out.println("Waiting to receive data...");
                 while (!reader.ready());
+
+                System.out.println("Receiving data...");
                 String fileName = null;
                 while (reader.ready()) {
                     String line = reader.readLine();
                     if(line.startsWith("GET")) {
                         String[] parse = line.split(" ");
                         if(parse[1].equals("/"))
-                            fileName = "/index.html";
-                        else
-                            fileName = parse[1];
+                            fileName = wwwPath + "/index.html";
+                        else {
+                            fileName = wwwPath + parse[1];
+                            if(parse[1].equals("/bye.html"))
+                                serverRun=false;
+                        }
                     }
                     System.out.println(line);
                 }
 
+                System.out.println("Sending a response");
                 List<String> outputData = new ArrayList<>();
-
-                try (Scanner sc = new Scanner(new File("www" + fileName))) {
+                try (Scanner sc = new Scanner(new File(fileName))) {
                     outputData.add("HTTP/1.1 200 OK");
                     outputData.add("Content-Type: text/html; charset=utf-8");
                     outputData.add("");
@@ -60,6 +71,9 @@ public class Server {
                     writer.println(data);
 
                 writer.flush();
+                outputData.clear();
+                reader.close();
+                writer.close();
                 socket.close();
             }
         } catch (IOException e) {
